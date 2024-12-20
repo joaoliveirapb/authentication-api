e8923224f8e92043bd5984c1960d1ccf507dec4b
joaoliveirapb <joaoliveira.batista1@gmail.com>
2024-12-20 09:33:54 -0300

    feat: Implement user creation route

--- src/http/server.ts
+++ src/http/server.ts
@@ -7,6 +7,7 @@ import {
   type ZodTypeProvider
 } from 'fastify-type-provider-zod'
 import { env } from '@/env'
+import { createUserRoute } from './routes/create-user'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -20,6 +21,8 @@ app.register(fastifyCors, {
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 })
+
+app.register(createUserRoute)
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
