8729a7d2a39bcf6dcd5e6e08ff681ff46113b334
joaoliveirapb <joaoliveira.batista1@gmail.com>
2024-12-20 15:06:22 -0300

    feat: Implement user authentication route

--- src/http/server.ts
+++ src/http/server.ts
@@ -8,6 +8,7 @@ import {
 } from 'fastify-type-provider-zod'
 import { env } from '@/env'
 import { createUserRoute } from './routes/create-user'
+import { authenticateRoute } from './routes/authenticate'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -20,9 +21,11 @@ app.register(fastifyCors, {
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  sign: { expiresIn: '1h' },
 })
 
 app.register(createUserRoute)
+app.register(authenticateRoute)
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
